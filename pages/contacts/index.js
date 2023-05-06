619b2199a82ba00bb994f86a5c19dfc193860a02
Kettengeschwindigkeit <666damned999@mail.ru>
2023-05-06 14:37:00 +0300

    Fetching Data (client)

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -1,13 +1,32 @@
+import { useEffect, useState } from "react";
 import Head from "next/head";
 import Heading from "../../components/Heading";
 
-const Contacts = () => (
-    <>
-        <Head>
-            <title>Contacts</title>
-        </Head>
-        <Heading text="Contacts list:" />
-    </>
-);
+const Contacts = () => {
+    const [contacts, setContacts] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch("https://jsonplaceholder.typicode.com/users");
+            const data = await response.json();
+            setContacts(data);
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <>
+            <Head>
+                <title>Contacts</title>
+            </Head>
+            <Heading text="Contacts list:" />
+            <ul>
+                {contacts && contacts.map(({ id, name, email }) => (
+                    <li key={id}><strong>{name}</strong> {email}</li>
+                ))}
+            </ul>
+        </>
+    )
+};
 
 export default Contacts;
